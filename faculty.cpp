909cbe083ae8b8dafae7383da38199ae51e7ade9
Rubayet Hasan <63944773+rubaith23@users.noreply.github.com>
2025-05-04 21:27:10 +0600

    more changes

--- faculty.cpp
+++ faculty.cpp
@@ -14,8 +14,9 @@ void faculty::menu()
         cout << "2. View Course Info\n";
         cout << "3. View Attendance\n";
         cout << "4. Update Attendance\n";
-        cout << "5. Input/Edit Grades\n";
-        cout << "6. Exit\n";
+        cout << "5. View Grades\n";
+        cout << "6. Add marks\n";
+        cout << "7. Exit\n";
         cin >> n;
         switch (n)
         {
@@ -34,8 +35,12 @@ void faculty::menu()
             updateattnd();
             break;
         case 5:
+            viewgrades();
             break;
         case 6:
+            addmarks();
+            break;
+        case 7:
             cout << "Logged Out!\n";
             Sleep(2000);
             return;
@@ -115,14 +120,17 @@ void faculty::updateattnd()
     ofstream file("./data/attendance.csv", ios_base::app);
 
     student var;
-    string x;
+    string x,t;
     for (int i = 0; i < var.students.size(); i++)
     {
         if (var.students[i][1] == course)
         {
+            timestamp = time(nullptr);
+            t = ctime(&timestamp);
+            t = t.substr(0,t.size() - 1);
             cout << "Enter 1 if " << var.students[i][0] << " is present or 0 if not!\n";
             cin >> x;
-            file << var.students[i][0] << ',' << course << ',' << "_time" << ',' << x << "\n";
+            file << var.students[i][0] << ',' << course << ',' << t << ',' << x << "\n";
         }
     }
     file.close();
@@ -130,8 +138,9 @@ void faculty::updateattnd()
 }
 void faculty::viewattnd()
 {
+    int total = 1;
     system("cls");
-    map<pair<string, string>, pair<float, float>> var;
+    map<pair<string, string>, pair<int, int>> var;
     vector<vector<string>> att;
     updatevector("attendance", att);
     for (int i = 0; i < att.size(); i++)
@@ -148,12 +157,126 @@ void faculty::viewattnd()
     cout << "Present_Percentage\n";
     for (auto i : var)
     {
+        total = max(i.second.first,total);
         cout << i.first.first << ",";
         cout << i.first.second << ",";
-        cout << i.second.first << ",";
-        cout << ceil(i.second.second * 100.00 / i.second.first) << "%\n";
+        cout << total<< ",";
+        cout << ceil(i.second.second * 100.00 / total) << "%\n";
     }
     string pp;
     cout << "\nEnter anything to exit\n";
     cin >> pp;
+}
+void faculty::addmarks()
+{
+    ofstream file("./data/marks.csv",ios_base::app);
+    vector<vector<string>>marks;
+    updatevector("marks",marks);
+    student var;
+    string mark,title;
+    int flag;
+    for (int i = 0; i < var.students.size(); i++)
+    {
+        if (var.students[i][1] == course)
+        {
+            flag = 1;
+            cout << "Enter marks for " << var.students[i][0] << " in course " <<course<<"\n";
+            
+            cin >> mark;
+            cout << "Enter exam title(mid/interim/final)\n";
+            cin >> title;
+            for(int j = 0; j < marks.size();j++)
+            {
+                // len = marks[i].size();
+                // for (int j = 0; j < len; j++)
+                if(marks[j][0] == var.students[i][0] && marks[j][3] == title)
+                {
+                    system("cls");
+                    cout << title << "-Mark already given for " << marks[j][0] << "\n";
+                    Sleep(2000);
+                    flag = 0;
+                    break;
+                }
+                
+            }
+            if(flag)
+            {
+                file <<  var.students[i][0] << ',' << course << ',' << mark << ',' << title << "\n"; 
+            }
+        }
+    }
+    file.close();
+}
+void faculty::addgrades()
+{
+    vector<vector<string>>marks;
+    updatevector("marks",marks);
+    
+    map<pair<string, string>, int> var;
+    for (int i = 0; i < marks.size(); i++)
+    {
+        int res = stod(marks[i][2]);
+        if(marks[i][3] == "interim")
+        {
+            res = res * (10.00 / 100.00);
+        }
+        else if(marks[i][3] == "mid")
+        {
+            res = res * (40.00/ 100.00);
+        }
+        else 
+        {
+            res = res * (50.00 / 100.00);
+        }
+        var[make_pair(marks[i][0], marks[i][1])] += res;    
+    }
+    ofstream file("./data/grades.csv");
+    for(auto i : var)
+    {
+        file << i.first.first << ',';
+        file << i.first.second << ',';
+        int m = i.second;
+        switch (m / 10)
+        {     
+        case 10:
+        case 9:   
+        case 8:
+            file << "A+" << "\n";
+            break;
+        case 7:
+            file << "A"<< "\n";
+            break;
+        case 6:
+            file << "B"<< "\n";
+            break;
+        case 5:
+            file << "C"<< "\n";  
+            break;
+        case 4:
+            file << "D"<< "\n";
+            break;        
+        default:
+            file << "F"<< "\n";
+            break;
+        }
+        
+    }
+    file.close();
+}
+void faculty::viewgrades()
+{
+    system("cls");
+    faculty obj;
+    grades = obj.grades;
+    for(auto i : grades)
+    {
+        for(auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << "\n";
+    }
+    string exit;
+    cout << "Enter anything to exit!\n";
+    cin >> exit;
 }
--- faculty.h
+++ faculty.h
@@ -5,29 +5,36 @@
 class faculty : public admin
 {
     string username, password,course;
+    time_t timestamp;
     vector<string> names = {"Course_ID", "Course_Name", "Faculty_Name"};
     void updateattnd();
     void viewattnd();
     void uploadcourseinfo();
+    void addmarks();
+    void addgrades();
     void viewgrades();
-    void inputgrades();
     void getcourseid();
+
     const vector<vector<string>> &viewcourses();
 
 public:
     vector<vector<string>> courses;
-
+    vector<vector<string>> grades;
     void viewcourseinfo();
     void menu() override;
     faculty()
     {
         updatevector("courses", courses);
+        updatevector("grades",grades);
+
         getcourseid();
     }
     faculty(string u)
     {
         username = u;
-        getcourseid();
+        addgrades();
         updatevector("courses", courses);
+        updatevector("grades",grades);
+        getcourseid();
     }
 };
